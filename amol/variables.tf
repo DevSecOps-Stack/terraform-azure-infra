9a102403d3db325004d87e395434943beac8038a
Rakesh <sharma742762@gmail.com>
2025-02-04 21:33:24 +0530

    fixed syntax error

--- amol/variables.tf
+++ amol/variables.tf
@@ -43,15 +43,8 @@ variable "os_disk_type" {
   default     = "Standard_LRS"
   description = "OS disk type for the VM."
 }
-variable "environment_name" {
-  type        = string
-  description = "Name of the environment (dev or prod)."
-}
-
 variable "location" {
   type        = string
   description = "Azure region where resources will be created."
 }
 
-// Add more variables as needed for AKS, VMs, etc.
->>>>>>> refs/remotes/origin/main
