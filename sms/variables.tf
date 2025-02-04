e4ca8f0c04374930db2ea543bef384a884222d8a
Rakesh <sharma742762@gmail.com>
2025-02-04 17:54:17 +0530

    Update variables.tf

---
+++ sms/variables.tf
@@ -0,0 +1,11 @@
+variable "environment_name" {
+  type        = string
+  description = "Name of the environment (dev or prod)."
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region where resources will be created."
+}
+
+// Add more variables as needed for your SMS project.
